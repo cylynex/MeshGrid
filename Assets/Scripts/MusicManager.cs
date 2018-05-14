db11aea49133334b70344bdb9713de4de595e0eb
Santo Commarato <cylynex@gmail.com>
2018-05-14 17:00:06 -0600

    sounds and resolution fixes

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+    
+    private AudioSource bgMusic;
+
+    void Awake() {
+        bgMusic = GetComponent<AudioSource>();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void OnLevelWasLoaded(int level) {
+        if (bgMusic) {
+            bgMusic.loop = true;
+            bgMusic.Play();
+        }
+    }
+
+    public void ChangeVolume(float volume) {
+        bgMusic.volume = volume;
+    }
+}
